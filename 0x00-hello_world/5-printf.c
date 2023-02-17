1fc4d289b82bb669aaa668860531c6dba340f81c
Mohamed Ahmed <m.ahmed141@hotmail.com>
2023-02-16 16:55:31 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prits a line using printf function
- * Describtion: " Aprogram"
  * Return: 0 (Success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+return(0);
 }
+
