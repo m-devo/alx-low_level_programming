543317e8fd01bddb789f1570c60288c7961cf330
Mohamed Ahmed <m.ahmed141@hotmail.com>
2023-02-16 16:27:01 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include stdio.h>
+/*
+ * main - A program that prints a line
+ * Return: 0 (Success)
+ */
+init main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
